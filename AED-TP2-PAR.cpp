102ccd55fda1433b5681b12cb71a9069ca95d72c
ggidoyaga <ggidoyaga@yahoo.com>
2015-04-23 23:56:33 -0300

    Update AED-TP2-PAR.cpp

--- AED-TP2-PAR.cpp
+++ AED-TP2-PAR.cpp
@@ -1,20 +1,19 @@
 /* AED-TP2-PAR
 * Gustavo Gaston Idoyaga
-* 20150422
+* 20150423
 */
 
 #include <iostream>
 
-int a, b, c;
+int a;
 
 int main () {
 	std :: cout << "Ingrese un numero.\n" ;
 	std :: cin >> a;
-	b = a % 2;
-	if (b == 0) {
+	if (a % 2 == 0) {
 		std :: cout << "El numero ingresado es par.\n";
 	}
 	else {
-		std :: cout << "El numero ingresado es impar.\n";
+		std :: cout << "El numero ingresado no es par.\n";
 	}
 }
